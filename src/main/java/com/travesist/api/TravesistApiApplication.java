4cc437d836ceaee20234dc954a1860db18b4c5fc
kittipong srikittichai <kittipong.sri@mail.kmutt>
2017-10-25 12:13:37 +0700

    inital project

---
+++ src/main/java/com/travesist/api/TravesistApiApplication.java
@@ -0,0 +1,12 @@
+package com.travesist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravesistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravesistApiApplication.class, args);
+	}
+}
